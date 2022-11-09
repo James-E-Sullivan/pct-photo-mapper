e586416e87e7911c90802a5ee1fe479da535ff03
James Sullivan <jesullivan222@gmail.com>
2022-11-09 14:29:49 -0500

    Add index view to views.py

--- photomapper/views.py
+++ photomapper/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import GeoImage
+
+def index(request):
+    # get all GeoImage objects from db
+    geo_image_queryset = GeoImage.objects.all()
+    geo_image_list = list(geo_image_queryset.values())
+
+    # return request, path, and context dict (to pass in objects)
+    return render(request, 'playground/index.html', {
+        'geo_image_queryset': geo_image_queryset,
+        'geo_image_list': geo_image_list,
+    })
