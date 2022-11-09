ffc56ab615d8f0664dc8ffca3af101762b0a5a95
James Sullivan <jesullivan222@gmail.com>
2022-11-09 15:16:07 -0500

    Add dependencies remove external js

--- pctsite/settings.py
+++ pctsite/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('DJANGO_SECRET_KEY')
+SECRET_KEY = 'django-insecure-akr%$&xmg12^l-8&-1s&i0motob6_z&j6f_!snmnfh6wh5lu!u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
